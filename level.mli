d14c8d1866daeff5efbfe99a4eae3b48cb517744
Andrew Xu <ax28@cornell.edu>
2017-11-11 15:59:47 -0500

    basic level structure

--- level.mli
+++ level.mli
@@ -1,1 +1,13 @@
 open Entities
+open State
+
+(*[objects] is the list of all entities on the current level.*)
+val objects : entity list
+
+(*[start_pos] is where the player starts off on the current level.*)
+val start_pos : position
+
+(*[exit] is the position the player needs to be in to continue to the next level.*)
+val exit : position
+
+val holes : position list
--- main.mli
+++ main.mli
@@ -4,3 +4,7 @@ open Entities
 (*[detect_hit h1,h2] return true if there exists an overlap of coordinates
   between hitboxes [h1] and [h2] and false otherwise.*)
 val detect_hit : hitbox * hitbox -> bool
+
+val is_game_over : bool
+
+val game_over : bool

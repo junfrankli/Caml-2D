f2fd4626ae9e60ac4b39120bb1b401d8cd7a3299
Andrew Xu <ax28@cornell.edu>
2017-11-11 15:46:57 -0500

    basic object collision

--- entities.mli
+++ entities.mli
@@ -1,2 +1,10 @@
+open State
+
 (*Represents an object in the game*)
 type entity
+
+val pos : position
+
+(*Pair of positions where the first is the bottom left corner of the hitbox
+  and the second is the top right corner.*)
+type hitbox = position * position
--- main.mli
+++ main.mli
@@ -1,1 +1,6 @@
 (*update physics based on command*)
+open Entities
+
+(*[detect_hit h1,h2] return true if there exists an overlap of coordinates
+  between hitboxes [h1] and [h2] and false otherwise.*)
+val detect_hit : hitbox * hitbox -> bool

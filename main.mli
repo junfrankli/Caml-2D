5d5e4cc524be4c13d950c501c9bc708e31ebf4ae
AndrewXu2020 <ax28@cornell.edu>
2017-11-13 16:18:17 -0500

    Update main.mli

--- main.mli
+++ main.mli
@@ -7,4 +7,6 @@ val game_over : bool
   between hitboxes [h1] and [h2] and false otherwise.*)
 val detect_hit : hitbox * (hitbox list) -> bool
 
+(*[main] will drive the whole game and should be what
+  initiates the game*)
 val main : unit -> unit

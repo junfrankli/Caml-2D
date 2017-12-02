b12b7aa759a02e33b1298a6c638dbecaa8922ad1
vagrant <vagrant@vm3110-2017fa>
2017-12-02 15:42:42 -0500

    begin implementing keypress events

--- state.ml
+++ state.ml
@@ -3,6 +3,14 @@ open Command
 
 type position = int * int
 
+(* keeps track of which keys are pressed down
+ * makes it easier to do physics *)
+type pressed = {
+  w_up : bool;
+  a_left : bool;
+  d_right : bool;
+  space_jump : bool}
+
 let vel = 0.0
 
 let acc = 0.0
@@ -23,3 +31,11 @@ let has_jump = if !num_jumps > 0 then true else false
 let level = 0
 
 let pos_list = []
+
+type state = {
+  pressed_keys : pressed;
+}
+
+let init_state ():state = {
+  pressed_keys = {w_up = false;a_left = false;d_right = false;space_jump = false};
+}

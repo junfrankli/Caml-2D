1627d88c0f927ca2dd1c7698057bf6435c0d95e7
vagrant <vagrant@vm3110-2017fa>
2017-12-02 14:40:51 -0500

    basic command.ml

--- command.ml
+++ command.ml
@@ -4,6 +4,15 @@ type input =
   | Jump
   | Shoot
 
-let check_input i = failwith "Unimplemented"
+let check_input i =
+  match i with
+  | Left, Right, Jump, Shoot -> true
+  | _ -> false
 
-let parse s = failwith "Unimplemented"
+let parse s =
+  let s' = String.(trim s |> lowercase_ascii) in
+  if s'="l" then Left
+  else if s'="r" then Right
+  else if s'="j" then Jump
+  else if s'="s" then Shoot
+  else failwith "invalid input"

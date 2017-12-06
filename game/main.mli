3a5d14672fed933f9727fcbfa515f3d2fec0d342
Jeff Liu <jl2964@cornell.edu>
2017-12-06 15:18:32 -0500

    commented out main b/c causing problems for camlimages

--- game/main.mli
+++ game/main.mli
@@ -1,4 +1,4 @@
-(*update physics based on command*)
+(*(*update physics based on command*)
 open Entities
 
 val game_over : bool
@@ -6,3 +6,4 @@ val game_over : bool
 (*[main] will drive the whole game and should be what
   initiates the game*)
 val main : unit -> unit
+*)
